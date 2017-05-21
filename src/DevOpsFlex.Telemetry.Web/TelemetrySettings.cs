c458d58838f37938f1101ddaaa41c47590266530
djfr <davidferreirarodrigues@gmail.com>
2017-05-22 00:02:07 +0100

    Added telemetry settings

---
+++ src/DevOpsFlex.Telemetry.Web/TelemetrySettings.cs
@@ -0,0 +1,18 @@
+﻿namespace DevOpsFlex.Telemetry.Web
+{
+    /// <summary>
+    /// Contains settings related to Telemetry.
+    /// </summary>
+    public class TelemetrySettings
+    {
+        /// <summary>
+        /// Gets and sets the main telemetry instrumentation key.
+        /// </summary>
+        public string InstrumentationKey { get; set; }
+
+        /// <summary>
+        /// Gets and sets the internal instrumentation key.
+        /// </summary>
+        public string InternalKey { get; set; }
+    }
+}
