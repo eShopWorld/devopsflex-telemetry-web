81b2ccb824ddae9bb77c42ab864fa2d3bd278ba9
djfr <davidferreirarodrigues@gmail.com>
2017-05-22 13:54:52 +0100

    Added an integration test to the middleware extension

---
+++ src/Tests/DevOpsFlex.Telemetry.Web.Tests/BigBrotherMiddlewareExtensionsTest.cs
@@ -0,0 +1,36 @@
+﻿namespace DevOpsFlex.Telemetry.Web.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Core;
+    using DevOpsFlex.Tests.Core;
+    using FluentAssertions;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.TestHost;
+    using Xunit;
+
+    public class BigBrotherMiddlewareExtensionsTest
+    {
+        [Fact, IsIntegration]
+        public async Task Test_Middleware_PushesToBigBrother()
+        {
+            var blewUp = false;
+            var server = new TestServer(
+                new WebHostBuilder().UseStartup<TestStartup>());
+
+            var client = server.CreateClient();
+            var (stream, _, _) = TestStartup.Bb;
+
+            using (stream.Subscribe(
+                e =>
+                {
+                    blewUp = true;
+                    e.Should().BeOfType<BbExceptionEvent>();
+                }))
+            {
+                await client.GetAsync("/");
+                blewUp.Should().BeTrue();
+            }
+        }
+    }
+}
---
+++ src/Tests/DevOpsFlex.Telemetry.Web.Tests/TestStartup.cs
@@ -0,0 +1,34 @@
+﻿namespace DevOpsFlex.Telemetry.Web.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class TestStartup
+    {
+        internal static readonly BigBrother Bb = new BigBrother("", "");
+
+        public TestStartup(IHostingEnvironment env)
+        {
+        }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IBigBrother>(Bb);
+            return services.BuildServiceProvider();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseBigBrotherExceptionHandler();
+
+            app.Run(async ctx =>
+            {
+                await Task.Yield();
+                throw new Exception("KABUM!!!");
+            });
+        }
+    }
+}
