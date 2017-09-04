f14c05cb843d0281658cd70cf24a04ac00d1aa83
Vaclav Haken <vaclav.haken@outlook.com>
2017-09-04 16:55:30 +0100

    fix candidate

--- src/Tests/DevOpsFlex.Telemetry.Web.Tests/BigBrotherExceptionMiddlewareTest.cs
+++ src/Tests/DevOpsFlex.Telemetry.Web.Tests/BigBrotherExceptionMiddlewareTest.cs
@@ -42,5 +42,33 @@ public class BigBrotherExceptionMiddlewareTest
 
             mockMiddleware.Verify(x => x.HandleException(It.IsAny<HttpContext>(), It.IsAny<Exception>()), Times.Once);
         }
+
+        [Fact, IsUnit]
+        public async Task Test_HandlesResponseStarted_GenericException()
+        {
+            var mockMiddleware = new Mock<BigBrotherExceptionMiddleware>(MockBehavior.Loose, new RequestDelegate(_ => throw new Exception("KABUM")), new Mock<IBigBrother>().Object);
+            mockMiddleware.Setup(x => x.Invoke(It.IsAny<HttpContext>())).CallBase();
+            mockMiddleware.Setup(x => x.HandleException(It.IsAny<HttpContext>(), It.IsAny<Exception>())).CallBase();
+
+            var mockHttpContext = new Mock<HttpContext>();
+            mockHttpContext.SetupSet<int>(context => context.Response.StatusCode = It.IsAny<int>()).Throws<InvalidOperationException>();
+
+            await mockMiddleware.Object.Invoke(mockHttpContext.Object);
+
+        }
+
+        [Fact, IsUnit]
+        public async Task Test_HandlesResponseStarted_BadRequestException()
+        {
+            var mockMiddleware = new Mock<BigBrotherExceptionMiddleware>(MockBehavior.Loose, new RequestDelegate(_ => throw new BadRequestException()), new Mock<IBigBrother>().Object);
+            mockMiddleware.Setup(x => x.Invoke(It.IsAny<HttpContext>())).CallBase();
+            mockMiddleware.Setup(x => x.HandleException(It.IsAny<HttpContext>(), It.IsAny<Exception>())).CallBase();
+
+            var mockHttpContext = new Mock<HttpContext>();
+            mockHttpContext.SetupSet<int>(context => context.Response.StatusCode = It.IsAny<int>()).Throws<InvalidOperationException>();
+
+            await mockMiddleware.Object.Invoke(mockHttpContext.Object);
+
+        }
     }
 }
