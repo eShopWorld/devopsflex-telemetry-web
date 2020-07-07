f6b5048d9a17ccefd4a8c1d082b9195e23f99074
David Guerin <david@developershelf.com>
2020-07-07 13:11:05 +0100

    Updating missing interface

--- src/Eshopworld.Web/EswSslExtentions.cs
+++ src/Eshopworld.Web/EswSslExtentions.cs
@@ -41,14 +41,15 @@ namespace Eshopworld.Web
         /// </summary>
         /// <param name="builder">The web host builder.</param>
         /// <param name="listener">The listener used to access the endpoints configuration.</param>
+        /// <param name="enableIpv6Binding">Switch to enable IPV6 binding - which is needed for the Service Fabric Proxy and Kestrel SSL binding to the app</param>
         /// <returns>The web host builder.</returns>
-        public static IWebHostBuilder UseEswSsl(this IWebHostBuilder builder, AspNetCoreCommunicationListener listener)
+        public static IWebHostBuilder UseEswSsl(this IWebHostBuilder builder, AspNetCoreCommunicationListener listener, bool enableIpv6Binding = false)
         {
             var ports = from endpoint in listener.ServiceContext.CodePackageActivationContext.GetEndpoints()
                         where endpoint.EndpointType == EndpointType.Input
                         where endpoint.Protocol == EndpointProtocol.Http || endpoint.Protocol == EndpointProtocol.Https
                         select (endpoint.Port, endpoint.Protocol == EndpointProtocol.Https);
-            return builder.UseEswSsl(ports);
+            return builder.UseEswSsl(ports, enableIpv6Binding);
         }
 
         /// <summary>
