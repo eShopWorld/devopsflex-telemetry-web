bea062aa01503c8fd47fa585153a79c69b4f320d
dawidkeshop <dkaliszewski@eshopworld.com>
2021-02-11 19:37:19 +0100

    BadRequestExceptionTest extended

--- src/Tests/Eshopworld.Web.Tests/BadRequestExceptionTest.cs
+++ src/Tests/Eshopworld.Web.Tests/BadRequestExceptionTest.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
 
 namespace Eshopworld.Web.Tests
 {
     using System.IO;
     using System.Linq;
-    using Eshopworld.Tests.Core;
     using System.Runtime.Serialization.Formatters.Binary;
+    using Eshopworld.Tests.Core;
     using FluentAssertions;
     using Microsoft.AspNetCore.Mvc.ModelBinding;
     using Xunit;
@@ -31,8 +33,6 @@ namespace Eshopworld.Web.Tests
 
                 var result = ex.ToResponse();
 
-                //result.Parameters.Should().HaveCount(3);
-
                 var expectedParams = new List<BadRequestParameter>
                 {
                     new BadRequestParameter { Name=nullParam, Description = $"Parameter {nullParam} should not be null."},
@@ -43,12 +43,6 @@ namespace Eshopworld.Web.Tests
                 result.Parameters.Should().BeEquivalentTo(expectedParams);
 
                 result.Caller.Should().BeNullOrEmpty();
-
-                //var firstParam = result.Parameters.First();
-
-                //result.Parameters.Select(p => p.Name).Should().Contain(nullParam);
-                //result.Parameters.Select(p => p.Name).Should().Contain(badFormatParam);
-                //result.Parameters.Select(p => p.Name).Should().Contain(genericBadParam);
             }
         }
 
@@ -79,6 +73,14 @@ namespace Eshopworld.Web.Tests
         public class ThrowIfInvalid
         {
             [Fact, IsLayer0]
+            public void Test_ModelState_WithNoError()
+            {
+                var ms = new ModelStateDictionary();
+
+                ms.Invoking(x => x.ThrowIfInvalid()).Should().NotThrow();
+            }
+
+            [Fact, IsLayer0]
             public void Test_ModelState_With1Error()
             {
                 const string errorKey = "my error key";
@@ -101,6 +103,75 @@ namespace Eshopworld.Web.Tests
                 }
 
                 threwBadRequest.Should().BeTrue();
+            }
+
+            [Fact, IsLayer0]
+            public void Test_ModelState_With1Exception()
+            {
+                const string errorKey = "my error key";
+                const string errorMsg = "my error message";
+
+                var threwBadRequest = false;
+                var ms = new ModelStateDictionary();
+                ms.AddModelError(errorKey, new Exception(errorMsg), new TestModelMetadata(ModelMetadataIdentity.ForType(typeof(string))));
+
+                try
+                {
+                    ms.ThrowIfInvalid();
+                }
+                catch (BadRequestException e)
+                {
+                    threwBadRequest = true;
+                    e.Parameters.Should().HaveCount(1);
+                    e.Parameters.FirstOrDefault().Key.Should().Be(errorKey);
+                    e.Parameters.FirstOrDefault().Value.Should().Be(errorMsg);
+                }
+
+                threwBadRequest.Should().BeTrue();
+            }
+
+            private class TestModelMetadata : ModelMetadata
+            {
+                public TestModelMetadata(ModelMetadataIdentity identity) : base(identity)
+                {
+                }
+
+                public override IReadOnlyDictionary<object, object> AdditionalValues { get; }
+                public override string BinderModelName { get; }
+                public override Type BinderType { get; }
+                public override BindingSource BindingSource { get; }
+                public override bool ConvertEmptyStringToNull { get; }
+                public override string DataTypeName { get; }
+                public override string Description { get; }
+                public override string DisplayFormatString { get; }
+                public override string DisplayName { get; }
+                public override string EditFormatString { get; }
+                public override ModelMetadata ElementMetadata { get; }
+                public override IEnumerable<KeyValuePair<EnumGroupAndName, string>> EnumGroupedDisplayNamesAndValues { get; }
+                public override IReadOnlyDictionary<string, string> EnumNamesAndValues { get; }
+                public override bool HasNonDefaultEditFormat { get; }
+                public override bool HideSurroundingHtml { get; }
+                public override bool HtmlEncode { get; }
+                public override bool IsBindingAllowed { get; }
+                public override bool IsBindingRequired { get; }
+                public override bool IsEnum { get; }
+                public override bool IsFlagsEnum { get; }
+                public override bool IsReadOnly { get; }
+                public override bool IsRequired { get; }
+                public override ModelBindingMessageProvider ModelBindingMessageProvider { get; }
+                public override string NullDisplayText { get; }
+                public override int Order { get; }
+                public override string Placeholder { get; }
+                public override ModelPropertyCollection Properties { get; }
+                public override IPropertyFilterProvider PropertyFilterProvider { get; }
+                public override Func<object, object> PropertyGetter { get; }
+                public override Action<object, object> PropertySetter { get; }
+                public override bool ShowForDisplay { get; }
+                public override bool ShowForEdit { get; }
+                public override string SimpleDisplayProperty { get; }
+                public override string TemplateHint { get; }
+                public override bool ValidateChildren { get; }
+                public override IReadOnlyList<object> ValidatorMetadata { get; }
             }
         }
 
